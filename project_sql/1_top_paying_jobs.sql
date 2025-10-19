eb1ca54e21537ee190fc6d8a119943640f702f92
michaelpod2006 <michael.pod2006@gmail.com>
2025-10-19 22:42:12 +0200

    Added a new file - top 10 most rewarding data analyst positions from the list

--- project_sql/1_top_paying_jobs.sql
+++ project_sql/1_top_paying_jobs.sql
@@ -1,1 +1,22 @@
---delete later
+/*
+Starting answering questions about:
+- Identifying the top 10 highest-paying Data Analyst roles that are available remotely.
+- Overviewing focuses on job posting with specified salaries (remove nulls).
+- Why? Highlighting the top-paying opportunities for Data Analyst, offering insights into empty
+*/
+
+SELECT 
+    job_title,
+    job_location,
+    salary_year_avg,
+    company_dim.name AS company_name
+FROM
+    job_postings_fact
+LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+WHERE
+    job_title_short = 'Data Analyst'
+    AND job_location = 'Anywhere'
+    AND salary_year_avg IS NOT NULL
+ORDER BY
+    salary_year_avg DESC
+LIMIT 10;
